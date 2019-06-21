55b32f6a89ea87c3cd292f42a39f2aeaf66fe22d
YougoLuck <good.luck.rika@gmail.com>
2019-06-21 16:42:15 +0900

    Add Model and bug fixed

--- Model.py
+++ Model.py
@@ -1,25 +1,24 @@
 from Preprocessor import  Preprocessor
-import random
 import numpy as np
+import tensorflow as tf
+
 
 class Model(object):
 
     def __init__(self):
         self.preProcessor = Preprocessor()
-        self.preProcessor.loadPreprocessedData(200)
-        self.Intdata, self.Label = self.shuffle(self.preProcessor.intData, self.preProcessor.allLabel)
-        self.splitTrainData()
         self.lstmSize = 256
         self.lstmLayers = 1
         self.batchSize = 500
-        self.learningRate = 0.001
+        self.learningRate = 0.0005
+        self.seqL = 100
 
 
     def splitTrainData(self):
-        splitFrac = 0.8
-        splitIdx = int(len(self.Intdata) * splitFrac)
-        trainX, valX = self.Intdata[:splitIdx], self.Intdata[splitIdx:]
-        trainY, valY = self.Label[:splitIdx], self.Label[splitIdx:]
+        splitFrac = 0.9
+        splitIdx = int(len(self.intData) * splitFrac)
+        trainX, valX = self.intData[:splitIdx], self.intData[splitIdx:]
+        trainY, valY = self.allLabel[:splitIdx], self.allLabel[splitIdx:]
 
         testIdx = int(len(valX) * 0.5)
         valX, testX = valX[:testIdx], valX[testIdx:]
@@ -35,16 +34,185 @@ class Model(object):
         print('testX:{}, testY:{}'.format(self.testX.shape, self.testY.shape))
         print('valX:{}, valY:{}'.format(self.valX.shape, self.valY.shape))
 
+    def getBatches(self, x, y, batchSize = 100):
+        nBatches = len(x) // batchSize
+        x, y = x[:nBatches * batchSize], y[:nBatches * batchSize]
+        for ii in range(0, len(x), batchSize):
+            yield x[ii:ii + batchSize], y[ii:ii + batchSize]
 
-    def shuffle(self, allData, allLabel):
-        allIndex = list(range(len(allData)))
-        random.shuffle(allIndex)
-        shuffleData = []
-        shuffleLabel = []
-        for index in allIndex:
-            shuffleData.append(allData[index])
-            shuffleLabel.append(allLabel[index])
-        return np.array(shuffleData), np.array(shuffleLabel)
+    def getCell(self):
+        return tf.contrib.rnn.BasicLSTMCell(self.lstmSize)
 
+    def bulidModel(self, batchSize, needFinalSigmoid=False):
+        nWords = len(self.preProcessor.intToVocab) + 500
+        embedSize = 300
+        graph = tf.Graph()
+        with graph.as_default():
+            inputs_ = tf.placeholder(tf.int32, [None, None], name = 'inputs')
+            labels_ = tf.placeholder(tf.int32, [None, None], name = 'labels')
+            keepProb = tf.placeholder(tf.float32, name = 'keep_prob')
 
-test = Model()
+            embedding = tf.Variable(tf.random_uniform((nWords, embedSize), -1, 1))
+            embed = tf.nn.embedding_lookup(embedding, inputs_)
+
+            cells = [tf.contrib.rnn.DropoutWrapper(tf.contrib.rnn.BasicLSTMCell(self.lstmSize), output_keep_prob = keepProb)
+                     for _ in range(self.lstmLayers)]
+            cell = tf.contrib.rnn.MultiRNNCell(cells)
+
+            # Getting an initial state of all zeros
+            initialState = cell.zero_state(batchSize, tf.float32)
+
+            outputs, finalState = tf.nn.dynamic_rnn(cell, embed,
+                                                     initial_state = initialState)
+
+            predictions = tf.contrib.layers.fully_connected(outputs[:, -1], 1)
+            if needFinalSigmoid:
+                predictions = tf.sigmoid(predictions)
+
+            cost = tf.losses.mean_squared_error(labels_, predictions)
+            optimizer = tf.train.AdamOptimizer(self.learningRate).minimize(cost)
+
+            return graph, inputs_, labels_, keepProb, cell, initialState, \
+                   finalState, cost, optimizer, predictions
+
+    def buildModel2Category(self, batchSize):
+        graph, inputs_, labels_, keepProb, cell, \
+        initialState, finalState, cost, optimizer, \
+        predictions = self.bulidModel(batchSize, True)
+        with graph.as_default():
+            correctPred = tf.equal(tf.cast(tf.round(predictions), tf.int32), labels_)
+            accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
+
+        return graph, inputs_, labels_, keepProb, cell, initialState, \
+               finalState, cost, optimizer, predictions, accuracy
+
+    def train(self, epochs):
+        self.intData, self.allLabel = self.preProcessor.loadPreprocessedData(self.seqL)
+        self.splitTrainData()
+        graph, inputs_, labels_, \
+        keepProb, cell, initialState, \
+        finalState, cost, optimizer, predictions = self.bulidModel(self.batchSize, False)
+
+        with graph.as_default():
+            saver = tf.train.Saver()
+
+        with tf.Session(graph = graph) as sess:
+            sess.run(tf.global_variables_initializer())
+            iteration = 1
+            lossHistory = []
+            valLossHistory = []
+            for e in range(epochs):
+                state = sess.run(initialState)
+                for ii, (x, y) in enumerate(self.getBatches(self.trainX, self.trainY, self.batchSize), 1):
+                    feed = {inputs_: x,
+                            labels_: y[:, None],
+                            keepProb: 0.5,
+                            initialState: state}
+                    loss, state, _ = sess.run([cost, finalState, optimizer], feed_dict = feed)
+                    lossHistory.append(loss)
+                    if iteration % 5 == 0:
+                        print("Epoch: {}/{}".format(e, epochs),
+                              "Iteration: {}".format(iteration),
+                              "Train loss: {:.3f}".format(loss))
+
+                    if iteration % 25 == 0:
+                        valState = sess.run(cell.zero_state(self.batchSize, tf.float32))
+                        valAllLoss = []
+                        for x, y in self.getBatches(self.valX, self.valY, self.batchSize):
+                            feed = {inputs_: x,
+                                    labels_: y[:, None],
+                                    keepProb: 1,
+                                    initialState: valState}
+                            valLoss, valState = sess.run([cost, finalState], feed_dict = feed)
+                            valAllLoss.append(valLoss)
+                        valMeanLoss = np.mean(valAllLoss)
+                        print("Epoch: {}/{}".format(e, epochs),
+                              "Iteration: {}".format(iteration),
+                              "val loss: {:.3f}".format(valMeanLoss))
+                        valLossHistory.append(valMeanLoss)
+                    iteration += 1
+                self.preProcessor.fileHandler.saveMetaFileHandler('./History/train_loss_epoch{}.txt'.format(e),
+                                                                  lossHistory)
+                self.preProcessor.fileHandler.saveMetaFileHandler('./History/val_loss_epoch{}.txt'.format(e),
+                                                                  valLossHistory)
+                saver.save(sess, "./checkpoints/sentiment.ckpt".format(e))
+
+    def train2Category(self, epochs):
+        self.intData, self.allLabel = self.preProcessor.loadPreprocessedData(self.seqL)
+        cnt = 0
+        for label in self.allLabel:
+            if label > 0:
+                cnt = cnt + 1
+        print('Label higher than threshold: {} lower: {}'.format(cnt, len(self.allLabel) - cnt))
+        self.splitTrainData()
+        graph, inputs_, labels_, \
+        keepProb, cell, initialState, \
+        finalState, cost, optimizer, \
+        predictions, accuracy = self.buildModel2Category(self.batchSize)
+
+        with graph.as_default():
+            saver = tf.train.Saver()
+
+        with tf.Session(graph = graph) as sess:
+            sess.run(tf.global_variables_initializer())
+            iteration = 1
+            lossHistory = []
+            accHistory = []
+            for e in range(epochs):
+                state = sess.run(initialState)
+                for ii, (x, y) in enumerate(self.getBatches(self.trainX, self.trainY, self.batchSize), 1):
+                    feed = {inputs_: x,
+                            labels_: y[:, None],
+                            keepProb: 0.5,
+                            initialState: state}
+                    loss, state, _ = sess.run([cost, finalState, optimizer], feed_dict = feed)
+                    lossHistory.append(loss)
+                    if iteration % 5 == 0:
+                        print("Epoch: {}/{}".format(e, epochs),
+                              "Iteration: {}".format(iteration),
+                              "Train loss: {:.3f}".format(loss))
+
+                    if iteration % 25 == 0:
+                        valState = sess.run(cell.zero_state(self.batchSize, tf.float32))
+                        valAllAcc = []
+                        for x, y in self.getBatches(self.valX, self.valY, self.batchSize):
+                            feed = {inputs_: x,
+                                    labels_: y[:, None],
+                                    keepProb: 1,
+                                    initialState: valState}
+                            valAcc, valState = sess.run([accuracy, finalState], feed_dict = feed)
+                            valAllAcc.append(valAcc)
+                        valMeanAcc = np.mean(valAllAcc)
+                        print("Epoch: {}/{}".format(e, epochs),
+                              "Iteration: {}".format(iteration),
+                              "Acc: {:.3f}".format(valMeanAcc))
+                        accHistory.append(valMeanAcc)
+                    iteration += 1
+                self.preProcessor.fileHandler.saveMetaFileHandler('./History/train_loss_epoch{}.txt'.format(e),
+                                                                  lossHistory)
+                self.preProcessor.fileHandler.saveMetaFileHandler('./History/val_acc_epoch{}.txt'.format(e),
+                                                                  accHistory)
+                saver.save(sess, "./checkpoints/sentiment.ckpt".format(e))
+
+    def predictAnime(self, synopses):
+        self.preProcessor.loadVocabToInt()
+        synopses = self.preProcessor.cleanUpData(synopses)
+        intSynopses = self.preProcessor.converDataToInt(synopses)
+        x = self.preProcessor.converIntDataToFeatures(intSynopses, self.seqL)
+
+        graph, inputs_, labels_, \
+        keepProb, cell, initialState, \
+        finalState, cost, optimizer, \
+        predictions, accuracy = self.buildModel2Category(len(x))
+
+        with graph.as_default():
+            saver = tf.train.Saver()
+        with tf.Session(graph = graph) as sess:
+            saver.restore(sess, tf.train.latest_checkpoint('checkpoints'))
+            testState = sess.run(cell.zero_state(len(x), tf.float32))
+            feed = {inputs_: x,
+                    keepProb: 1,
+                    initialState: testState}
+            result, _ = sess.run([tf.round(predictions), finalState], feed_dict = feed)
+            print('result:{}'.format(result))
+
--- Preprocessor.py
+++ Preprocessor.py
@@ -3,6 +3,7 @@ from collections import Counter
 import os
 import re
 import numpy as np
+import random
 
 class Preprocessor(object):
 
@@ -10,11 +11,10 @@ class Preprocessor(object):
         self.malHandler = MALHandler()
         self.fileHandler = FileHandler()
         self.savePath = '.'
-        self.allData = []
-        self.allLabel = []
         self.intData = []
+        self.allLabel = []
         self.vocabToInt = {}
-        self.IntToVocab = {}
+        self.intToVocab = {}
 
 
     def loadAllMetaDataAndLabel(self):
@@ -29,8 +29,7 @@ class Preprocessor(object):
                 data, label = self.malHandler.loadData(year, season)
                 allData = allData + data
                 allLabel = allLabel + label
-        self.allData = allData
-        self.allLabel = allLabel
+        return allData, allLabel
 
     def savePreprocessedData(self):
         self.fileHandler.saveMetaFileHandler('{}/preprocessed_data.txt'.format(self.savePath),
@@ -43,15 +42,46 @@ class Preprocessor(object):
     def loadPreprocessedData(self, seqLen):
         intData = self.fileHandler.loadMetaFileHandler('{}/preprocessed_data.txt'.format(self.savePath))
         allLabel = self.fileHandler.loadMetaFileHandler('{}/preprocessed_label.txt'.format(self.savePath))
-        vocabToInt = self.fileHandler.loadMetaFileHandler('{}/vocab_to_int.txt'.format(self.savePath))
+        print('Data cnt before Clean up: {}'.format(len(allLabel)))
+        self.loadVocabToInt()
         if len(intData) != len(allLabel):
             raise RuntimeError('Input and label numbers not match!')
+
+        self.intData = self.converIntDataToFeatures(intData, seqLen)
+        self.allLabel = np.array(allLabel, dtype = np.float)
+        return self.intData, self.allLabel
+
+
+    def generateVocabToInt(self, allData):
+        data = ' '.join(allData)
+        words = data.split()
+        counts = Counter(words)
+        vocab = sorted(counts, key = counts.get, reverse = True)
+        vocabToInt = {word: ii for ii, word in enumerate(vocab, 1)}
         self.vocabToInt = vocabToInt
+        self.getIntToVocab()
+
+    def loadVocabToInt(self):
+        path = '{}/vocab_to_int.txt'.format(self.savePath)
+        vocabToInt = self.fileHandler.loadMetaFileHandler(path)
+        self.vocabToInt = vocabToInt
+        self.getIntToVocab()
+
+    def getIntToVocab(self):
+        intToVocab = {value: key for key, value in self.vocabToInt.items()}
+        self.intToVocab = intToVocab
+
+    def converDataToInt(self, allData):
+        intData = []
+        for data in allData:
+            intData.append([self.vocabToInt[word] for word in data.split() if word in self.vocabToInt])
+        return intData
+
+    def converIntDataToFeatures(self, intData, seqLen):
         features = np.zeros((len(intData), seqLen), dtype = int)
         for i, row in enumerate(intData):
             features[i, -len(row):] = np.array(row)[:seqLen]
-        self.intData = features
-        self.allLabel = np.array(allLabel, dtype = np.float)
+        return features
 
     def removeSourceTag(self, data):
         index = data.find('(Source')
@@ -63,51 +93,84 @@ class Preprocessor(object):
             data = data[:index]
         return data
 
-    def removeShortData(self, threshold):
-        allData = []
-        allLabel = []
-        for i in range(len(self.allData)):
-            data = self.allData[i]
+    def removeShortData(self, allData, allLabel, threshold):
+        newData = []
+        newLabel = []
+        for i in range(len(allData)):
+            data = allData[i]
             dataArr = data.split(' ')
-            if len(dataArr) >= threshold and self.allLabel[i] != 'None':
-                allData.append(data)
-                allLabel.append(self.allLabel[i])
-        self.allLabel = allLabel
-        self.allData = allData
+            if len(dataArr) >= threshold and allLabel[i] != 'None':
+                newData.append(data)
+                newLabel.append(allLabel[i])
+        return newData, newLabel
 
-    def cleanUpData(self):
-        allData = []
+    def cleanUpData(self, allData):
+        newData = []
         rule = re.compile(r"[^a-zA-Z ]|( *$)")
-        for data in self.allData:
+        for data in allData:
             data = self.removeSourceTag(data)
             data = rule.sub(' ', data)
             data = data.lower()
-            allData.append(data)
-        self.allData = allData
+            newData.append(data)
+        return newData
 
-    def increaseLabelScale(self, scale):
-        allLabel = [float(label) * scale for label in self.allLabel]
-        self.allLabel = allLabel
+    def increaseLabelScale(self, allLabel, scale):
+        newLabel = [float(label) * scale for label in allLabel]
+        return newLabel
 
-    def generateW2V(self):
-        data = ' '.join(self.allData)
-        words = data.split()
-        counts = Counter(words)
-        vocab = sorted(counts, key = counts.get, reverse = True)
-        vocabToInt = {word: ii for ii, word in enumerate(vocab, 1)}
-        self.vocabToInt = vocabToInt
-        intData = []
-        for data in self.allData:
-            intData.append([vocabToInt[word] for word in data.split()])
-        self.intData = intData
+    def converLabelTo2Category(self, allLabel, threshold):
+        newLabel = [1 if float(label) > threshold else 0 for label in allLabel]
+        return newLabel
 
 
-    def run(self, dataThreshold, labelScale):
-        self.loadAllMetaDataAndLabel()
-        self.cleanUpData()
-        self.removeShortData(dataThreshold)
-        self.increaseLabelScale(labelScale)
-        self.generateW2V()
+    def shuffle(self, intData, allLabel):
+        allIndex = list(range(len(intData)))
+        random.shuffle(allIndex)
+        shuffleData = []
+        shuffleLabel = []
+        for index in allIndex:
+            shuffleData.append(intData[index])
+            shuffleLabel.append(allLabel[index])
+        return shuffleData, shuffleLabel
+
+
+    def runIntial(self, dataThreshold, labelScale):
+
+        #加载原始数据
+        allData, allLabel = self.loadAllMetaDataAndLabel()
+        #过滤数据
+        allData = self.cleanUpData(allData)
+        allData, allLabel = self.removeShortData(allData, allLabel, dataThreshold)
+        #生成vocab
+        self.generateVocabToInt(allData)
+        #转换数据到Int
+        intData = self.converDataToInt(allData)
+        #加大scale
+        allLabel = self.increaseLabelScale(allLabel, labelScale)
+        #shuffle
+        self.intData, self.allLabel = self.shuffle(intData, allLabel)
+        #保存
         self.savePreprocessedData()
 
+
+    def runIntial2Category(self, dataThreshold, labelThreshold):
+        #加载原始数据
+        allData, allLabel = self.loadAllMetaDataAndLabel()
+        #过滤数据
+        allData = self.cleanUpData(allData)
+        allData, allLabel = self.removeShortData(allData, allLabel, dataThreshold)
+        #生成vocab
+        self.generateVocabToInt(allData)
+        #转换数据到Int
+        intData = self.converDataToInt(allData)
+        #加大scale
+        allLabel = self.converLabelTo2Category(allLabel, labelThreshold)
+        #shuffle
+        self.intData, self.allLabel = self.shuffle(intData, allLabel)
+        #保存
+        self.savePreprocessedData()
+
+
+test = Preprocessor()
+test.runIntial2Category(20, 6.5)
 
